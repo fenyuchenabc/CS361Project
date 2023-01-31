aa34df8f947773db0d05a7c9227c2b0b51a10794
fenyuchenabc <fenyuchenabc@hotmail.com>
2023-01-31 17:08:12 -0500

    main py file update, using object-oriented to implement the function. Get properties of ui file.

--- first_python.py
+++ first_python.py
@@ -3,14 +3,32 @@ using qt designer to load ui
 """
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import uic
+
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = None
+        self.init_ui()
+
+    def init_ui(self):
+        """
+        get all the widgets from ui and bind signals and slots if needed
+        """
+        # load ui file
+        self.ui = uic.loadUi("./HomeScreen.ui")
+        inventory_btn = self.ui.pushButton
+        profile_btn = self.ui.pushButton_2
+        get_cards_btn = self.ui.pushButton_4
+        resources_btn = self.ui.pushButton_3
+        help_btn = self.ui.pushButton_5
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ui = uic.loadUi("./HomeScreen.ui")
-    ui.show()
+    # make a new window using MyWindow class
+    w = MyWindow()
+    w.ui.show()
     app.exec()
-
-
-
